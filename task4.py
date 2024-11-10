e8810728b1ae19be2373965a562047d561ab2c0a
RusAntonSazonov <115030299+RusAntonSazonov@users.noreply.github.com>
2024-11-10 21:56:09 +0300

    Add files via upload
    datas.py is recording of a lecture, other files are home tasks

---
+++ task4.py
@@ -0,0 +1,7 @@
+# 4th program
+print(float('123.456'))
+print(type(float('123.456')))
+print(float('123.456') * 10)
+print(int(float('123.456') * 10))
+print(int(float('123.456') * 10) % 10)
+print(type(int(float('123.456') * 10) % 10))
